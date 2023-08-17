b95a218eeea2b9fa1a49f3bea0229e2c2c61610d
Isaac <isaactorres778@gmail.com>
2023-08-17 18:26:34 +0000

    Updating file

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -2,8 +2,10 @@
 """
 function that determines if object is instance of a class that is inherited
 """
+
+
 def inherits_from(obj, a_class):
     """
     returns true or false
     """
-    return issubclass(obj, a_class) if type(obj) != a_class
+    return issubclass(obj, a_class) and obj != a_class
