02b2bd14715bc37e1ca488600823c0fc8c773f93
Isaac <isaactorres778@gmail.com>
2023-08-17 18:10:52 +0000

    Updating file

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -6,4 +6,4 @@ def inherits_from(obj, a_class):
     """
     returns true or false
     """
-    return issubclass(obj, a_class) if obj != a_class
+    return issubclass(obj, a_class) if type(obj) != a_class
