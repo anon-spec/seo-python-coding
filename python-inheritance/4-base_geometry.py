a88facc1b2d90391821a64caf082e6cec2d32cd6
Isaac <isaactorres778@gmail.com>
2023-08-17 20:17:09 +0000

    Adding file

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+function for empty class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ empty """
+    def area(self):
+        """Area of self"""
+        raise Exception("area() not implemented")
