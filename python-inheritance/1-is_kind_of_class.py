a938aa79a3f00848bb04a1093e41c63791e62064
Isaac <isaactorres778@gmail.com>
2023-08-17 16:47:20 +0000

    Adding file

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that determines if object is instance of specific class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    returns true or false
+    """
+    return issubclass(obj, a_class)
