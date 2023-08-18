9754732b7ca8d7968ae8f277719745ad0c355974
Isaac <isaactorres778@gmail.com>
2023-08-18 04:10:35 +0000

    Updating file

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """initialization of objects"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
