3165cc3a45ca8dcc5358aefd439b09f52ceed86f
Isaac <isaactorres778@gmail.com>
2023-08-18 03:49:46 +0000

    Updating file

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -37,4 +37,5 @@ class Rectangle(BaseGeometry):
         """Printing String"""
         a = str(self.__width)
         b = str(self.__height)
-        return "[Rectangle] " + a + "/" + b
+        return "[" + __class__.__name__ + "] " \
+            + a + "/" + b
