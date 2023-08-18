bbdbe5df3cf23fca476b19371775ec54a1a54296
Isaac <isaactorres778@gmail.com>
2023-08-18 00:06:34 +0000

    Updating file

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -2,25 +2,11 @@
 """MODULE DOCSTRING"""
 
 
-class BaseGeometry:
-    """
-    finds area and value of shape
-    """
-
-    def area(self):
-        """Area of self"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates value"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Subclass inherited from BaseGeometry"""
+"""Subclass inherited from BaseGeometry"""
 
     def __init__(self, width, height):
         """initialization of objects"""
