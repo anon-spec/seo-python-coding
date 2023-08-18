f13d0a23f4d10123dd34a414d58277b67262e6e9
Isaac <isaactorres778@gmail.com>
2023-08-18 02:42:10 +0000

    Updating file

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""MODULE DOCSTRING"""
+"""
+    class Rectangle that inherits from BaseGeometry
+    (7-base_geometry.py).
+
+    Instantiation with width and height:
+    def __init__(self, width, height):
+        width and height must be private.
+        No getter or setter
+        width and height must be positive integers,
+        validated by integer_validator
+"""
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-"""Subclass inherited from BaseGeometry"""
+    """ rectangle class inherits from basegeometry """
 
     def __init__(self, width, height):
-        """initialization of objects"""
-        super().integer_validator("width", width)
+        """ initialization """
         super().integer_validator("height", height)
-        self.__width = width
+        super().integer_validator("width", width)
         self.__height = height
+        self.__width = width
