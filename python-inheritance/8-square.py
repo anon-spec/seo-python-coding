4a18b6cdc36a663f01801878a496a338093a9783
Isaac <isaactorres778@gmail.com>
2023-08-19 22:47:34 +0000

    Updating file

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""MODULE DOCSTRING"""
+
+
+class BaseGeometry:
+    """
+    finds area and value of shape
+    """
+
+    def area(self):
+        """Area of self"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Subclass inherited from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """initialization of objects"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Solving area of Rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returning String"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """Subclass inherited from Rectangle"""
+
+    def __init__(self, size):
+        """initializing with size object"""
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """"solving area of square"""
+        return self.__size * self.__size
